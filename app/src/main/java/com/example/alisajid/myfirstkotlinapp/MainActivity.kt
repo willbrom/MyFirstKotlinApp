42e53a1544ab6fa4fd121b0f11632db462c9941a
willbrom <alisajid552@yahoo.com>
2018-09-18 13:00:32 +0300

    add count functionality

--- app/src/main/java/com/example/alisajid/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/alisajid/myfirstkotlinapp/MainActivity.kt
@@ -17,4 +17,18 @@ class MainActivity : AppCompatActivity() {
         val myToast = Toast.makeText(this, resources.getString(R.string.toast_message), Toast.LENGTH_SHORT)
         myToast.show()
     }
+
+    fun countMe(view: View) {
+
+        // get the value from textView
+        val countTextView = findViewById<TextView>(R.id.textView_count) as TextView
+        val countString = countTextView.text.toString()
+
+        // convert value to an integer and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // set the new value to textView
+        countTextView.text = count.toString()
+    }
 }
